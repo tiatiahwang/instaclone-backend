f4d314d4254b30be816f79535738e8556433f524
Tia Hwang <tiajshwang@gmail.com>
2021-09-14 15:05:22 +0900

    1.1 BackEnd - Apollo/Prisma

---
+++ server.js
@@ -0,0 +1,12 @@
+require("dotenv").config();
+
+import { ApolloServer } from "apollo-server";
+import schema from "./schema";
+
+const server = new ApolloServer({
+    schema,
+});
+
+const PORT = process.env.PORT;
+
+server.listen(PORT).then(() => console.log(`Server is Running on http://localhost:${PORT}`));
