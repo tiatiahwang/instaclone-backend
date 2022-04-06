ad106d649debc54de750d64fdff4dcd687c282db
tiahwang44 <tiahwang44@gmail.com>
2022-04-07 01:02:34 +0900

    FIX: heroku deploy error again

--- src/server.ts
+++ src/server.ts
@@ -10,6 +10,8 @@ import client from './client';
 const apollo = new ApolloServer({
   resolvers,
   typeDefs,
+  playground: true,
+  introspection: true,
   context: async ({ req }) => {
     return {
       loggedInUser: await getUser(req.headers.token),
