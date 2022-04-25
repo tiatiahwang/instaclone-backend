d06fa0a30432620a88aecbfc145eb636017acb46
tiahwang44 <tiahwang44@gmail.com>
2022-04-25 22:18:02 +0900

    UPDATE: infinite scrolling

--- src/photos/seeFeed/seeFeed.resolvers.ts
+++ src/photos/seeFeed/seeFeed.resolvers.ts
@@ -4,8 +4,10 @@ import { protectedResolver } from '../../users/users.utils';
 const resolvers: Resolvers = {
   Query: {
     seeFeed: protectedResolver(
-      async (_, __, { loggedInUser, client }) =>
+      async (_, { offset }, { loggedInUser, client }) =>
         await client.photo.findMany({
+          take: 2,
+          skip: offset,
           where: {
             OR: [
               { user: { followers: { some: { id: loggedInUser.id } } } },
